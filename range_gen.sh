7d76f27eb8d2f714c1e741f6d66cb9193f42ce6b
Maxwell BURSON <mburson@e1z3r9p17.42.us.org>
2017-03-21 09:43:06 -0700

    fixed range_gen.sh

--- range_gen.sh
+++ range_gen.sh
@@ -10,7 +10,7 @@ awk -v loop=$1 'BEGIN{
        count++
     }
   } while (count<loop)
-}'
+}' | tr '\n' ' '
 
 #shuf() { awk 'BEGIN {srand(); OFMT="%.17f"} {print rand(), $0}' "$@" |sort -k1,1n | cut -d ' ' -f2-; };
 #seq 1 $1 | shuf | tr '\n' ' '
